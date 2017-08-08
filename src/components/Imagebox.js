a1143bc799a56f9970f66a002045011f0c6995a4
Inseo B <yunibaek@hotmail.com>
2017-08-07 22:05:42 -0400

    Imagebox: add css styling to image

--- src/components/Imagebox.js
+++ src/components/Imagebox.js
@@ -1,28 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import "./stylesheet/imagebox.css";
 
 function Imagebox(props) {
   return (
-    <div style={styles.container}>
-      <img style={styles.image} {...props} />
+    <div className="container">
+      <img {...props} />
     </div>
   );
 }
-
-var styles = {
-  container: {
-    maxWidth: "700px",
-    maxHeight: "900px",
-    margin: "50px",
-    border: "5px solid black",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  image: {
-    width: "100%",
-    height: "auto"
-  }
-};
 
 export default Imagebox;
