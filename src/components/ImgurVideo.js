bb0955a06e800217861b24cd6bc56e2c4e4fe906
Inseo B <yunibaek@hotmail.com>
2017-08-11 13:53:15 -0400

    Imgur Video: set webkit plays in line attribute

--- src/components/ImgurVideo.js
+++ src/components/ImgurVideo.js
@@ -9,6 +9,13 @@ function cleanToMP4(url) {
   return url.replace(".gifv", ".mp4");
 }
 
+// get raw dom to set webkit plays in line attribute
+function attachCustomAttributes(domNode) {
+  if (domNode) {
+    domNode.setAttribute("webkit-playsinline", true);
+  }
+}
+
 function ImgurVideo({ src, ...props }) {
   return (
     <video
@@ -16,6 +23,7 @@ function ImgurVideo({ src, ...props }) {
       autoPlay="autoplay"
       muted="muted"
       loop="loop"
+      ref={attachCustomAttributes}
       {...props}
     >
       <source src={cleanToMP4(src)} type="video/mp4" />
