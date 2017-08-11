95b9a915d12d1c474d2ae8aac93caa337c32cf75
Inseo B <yunibaek@hotmail.com>
2017-08-11 13:40:25 -0400

    Fetch Posts: fetch posts from superaww reddit

--- src/utils/fetchPosts.js
+++ src/utils/fetchPosts.js
@@ -12,19 +12,22 @@ function normalizePost(post) {
 
 // Fetch a subreddit's top post
 function fetchTopPosts(subreddit) {
-  return fetch(`https://www.reddit.com/r/${subreddit}/top.json`)
-    // Parse response as JSON
-    .then(res => res.json())
-    // Cleanup big JSON mess into posts
-    .then(payload => {
-      return payload.data.children.map(child => normalizePost(child.data));
-    })
-    // catch any errors
-    .catch(err => {
-      console.log("parsing failed", err);
-    });
+  return (
+    //fetch(`https://www.reddit.com/r/${subreddit}/top.json?limit=100`)
+    fetch("https://www.reddit.com/user/316nuts/m/superaww/top.json?limit=100")
+      // Parse response as JSON
+      .then(res => res.json())
+      // Cleanup big JSON mess into posts
+      .then(payload => {
+        return payload.data.children.map(child => normalizePost(child.data));
+      })
+      // catch any errors
+      .catch(err => {
+        console.log("parsing failed", err);
+      })
+  );
 }
 
 export default function fetchPosts() {
-  return fetchTopPosts("aww");
+  return fetchTopPosts();
 }
